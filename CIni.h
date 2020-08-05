e9bff3543114e2dc3419ca21b69e9641158db0e5
QuickNET-Tech <timothy.quick.us.il@gmail.com>
2020-08-05 01:12:33 -0500

    Version 1.2.0 - new constructor, way more fucking const, little less std::string, adjustable buffer size, redundant code removal
    new constructor, way more fucking const, little less std::string, adjustable buffer size, redundant code removal

--- CIni.h
+++ CIni.h
@@ -1,16 +1,20 @@
 #pragma once
 
+#ifndef CINI_BUFFER_SIZE
+#define CINI_BUFFER_SIZE 0xFF
+#endif
+
+
 class CIni {
 public:
-	explicit CIni(const std::string& file) {
-		iniFile = file;
-	}
+	explicit CIni(const std::string& file) : iniFile(file.data()) {}
+	explicit CIni(const char* file) : iniFile(file) {}
 
 	/* --------------- SET --------------- */
 
-	bool setString(const std::string& string, const std::string& app, const std::string& key) {
+	bool setString(const std::string& string, const std::string& app, const std::string& key) const {
 		try {
-			WritePrivateProfileStringA(app.c_str(), key.c_str(), string.c_str(), iniFile.c_str());
+			WritePrivateProfileStringA(app.c_str(), key.c_str(), string.c_str(), iniFile);
 			return true;
 		}
 		catch(...) {
@@ -18,74 +22,38 @@ public:
 		}
 	}
 
-	bool setInt32(int value, const std::string& app, const std::string& key) {
-		try {
-			setString(std::to_string(value), app, key);
-			return true;
-		}
-		catch(...) {
-			return false;
-		}
+	bool setInt32(const int32_t value, const std::string& app, const std::string& key) const {
+		return setString(std::to_string(value), app, key);
 	}
 
-	bool setUint32(uint32_t value, const std::string& app, const std::string& key) {
-		try {
-			setString(std::to_string(value), app, key);
-			return true;
-		}
-		catch(...) {
-			return false;
-		}
+	bool setUint32(const uint32_t value, const std::string& app, const std::string& key) const {
+		return setString(std::to_string(value), app, key);
 	}
 
-	bool setInt64(int64_t value, const std::string& app, const std::string& key) {
-		try {
-			setString(std::to_string(value), app, key);
-			return true;
-		}
-		catch(...) {
-			return false;
-		}
+	bool setInt64(const int64_t value, const std::string& app, const std::string& key) const {
+		return setString(std::to_string(value), app, key);
 	}
 
-	bool setUint64(uint64_t value, const std::string& app, const std::string& key) {
-		try {
-			setString(std::to_string(value), app, key);
-			return true;
-		}
-		catch(...) {
-			return false;
-		}
+	bool setUint64(const uint64_t value, const std::string& app, const std::string& key) const {
+		return setString(std::to_string(value), app, key);
 	}
 
-	bool setFloat(float value, const std::string& app, const std::string& key) {
-		try {
-			setString(std::to_string(value), app, key);
-			return true;
-		}
-		catch(...) {
-			return false;
-		}
+	bool setFloat(const float value, const std::string& app, const std::string& key) const {
+		return setString(std::to_string(value), app, key);
 	}
 
-	bool setBool(bool value, const std::string& app, const std::string& key) {
-		try {
-			setString((value ? "1" : "0"), app, key);
-			return true;
-		}
-		catch(...) {
-			return false;
-		}
+	bool setBool(const bool value, const std::string& app, const std::string& key) const {
+		return setString(std::to_string(value), app, key);
 	}
 
 	/* --------------- GET --------------- */
 
 	// On failure, if write on fail is true, failvalue will be written at the specified app and key.
 	// After that failValue will be returned, even if it wasn't written.
-	std::string getString(const std::string& app, const std::string& key, const std::string& failValue, bool writeOnFail) {
+	std::string getString(const std::string& app, const std::string& key, const std::string& failValue, const bool writeOnFail, const uint16_t bufferSize = 0xFF) const {
 		try {
-			char buf[100];
-			GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
+			char buf[CINI_BUFFER_SIZE];
+			GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, sizeof(buf), iniFile);
 			return buf;
 		}
 		catch(...) {
@@ -98,151 +66,91 @@ public:
 
 	// On failure, if write on fail is true, failvalue will be written at the specified app and key.
 	// After that failValue will be returned, even if it wasn't written.
-	int getInt32(const std::string& app, const std::string& key, int failValue, bool writeOnFail) {
-		try {
-			char buf[100];
-			GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
-			return std::stoi(buf);
-		}
-		catch(...) {
-			if(writeOnFail) {
-				setInt32(failValue, app, key);
-			}
-			return failValue;
-		}
+	int getInt32(const std::string& app, const std::string& key, int failValue, const bool writeOnFail) const {
+		return std::stoi(getString(app, key, std::to_string(failValue), writeOnFail));
 	}
 
 	// On failure, if write on fail is true, failvalue will be written at the specified app and key.
 	// After that failValue will be returned, even if it wasn't written.
-	uint32_t getUint32(const std::string& app, const std::string& key, int failValue, bool writeOnFail) {
-		try {
-			char buf[100];
-			GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
-			return std::strtoul(buf, nullptr, 0);
-		}
-		catch(...) {
-			if(writeOnFail) {
-				setUint32(failValue, app, key);
-			}
-			return failValue;
-		}
+	uint32_t getUint32(const std::string& app, const std::string& key, const int failValue, const bool writeOnFail) const {
+		return std::stoi(getString(app, key, std::to_string(failValue), writeOnFail));
 	}
 
 	// On failure, if write on fail is true, failvalue will be written at the specified app and key.
 	// After that failValue will be returned, even if it wasn't written.
-	int64_t getInt64(const std::string& app, const std::string& key, int failValue, bool writeOnFail) {
-		try {
-			char buf[100];
-			GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
-			return std::stoll(buf);
-		}
-		catch(...) {
-			if(writeOnFail) {
-				setInt64(failValue, app, key);
-			}
-			return failValue;
-		}
+	int64_t getInt64(const std::string& app, const std::string& key, const int failValue, const bool writeOnFail) const {
+		return std::stoi(getString(app, key, std::to_string(failValue), writeOnFail));
 	}
 
 	// On failure, if write on fail is true, failvalue will be written at the specified app and key.
 	// After that failValue will be returned, even if it wasn't written.
-	uint64_t getUint64(const std::string& app, const std::string& key, int failValue, bool writeOnFail) {
-		try {
-			char buf[100];
-			GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
-			return std::stoull(buf);
-		}
-		catch(...) {
-			if(writeOnFail) {
-				setUint64(failValue, app, key);
-			}
-			return failValue;
-		}
+	uint64_t getUint64(const std::string& app, const std::string& key, const int failValue, const bool writeOnFail) const {
+		return std::stoi(getString(app, key, std::to_string(failValue), writeOnFail));
 	}
 
 	// On failure, if write on fail is true, failvalue will be written at the specified app and key.
 	// After that failValue will be returned, even if it wasn't written.
-	float getFloat(const std::string& app, const std::string& key, float failValue, bool writeOnFail) {
-		try {
-			char buf[100];
-			GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
-			return std::stof(buf);
-		}
-		catch(...) {
-			if(writeOnFail) {
-				setFloat(failValue, app, key);
-			}
-			return failValue;
-		}
+	float getFloat(const std::string& app, const std::string& key, const float failValue, const bool writeOnFail) const {
+		return std::stoi(getString(app, key, std::to_string(failValue), writeOnFail));
 	}
 
 	// On failure, if write on fail is true, failvalue will be written at the specified app and key.
 	// After that failValue will be returned, even if it wasn't written.
-	bool getBool(const std::string& app, const std::string& key, bool failValue, bool writeOnFail) {
-		try {
-			char buf[100];
-			GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
-			return (std::stoi(buf) ? true : false);
-		}
-		catch(...) {
-			if(writeOnFail) {
-				setBool(failValue, app, key);
-			}
-			return failValue;
-		}
+	bool getBool(const std::string& app, const std::string& key, const bool failValue, const bool writeOnFail) const {
+		return std::stoi(getString(app, key, std::to_string(failValue), writeOnFail));
 	}
 
 	/* --------------- UNSAFE GET ---------------*/
 
 	// No error handling, only use if you plan to handle errors on your own
-	std::string getStringUnsafe(const std::string& app, const std::string& key) {
-		char buf[100];
-		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
+	std::string getStringUnsafe(const std::string& app, const std::string& key) const {
+		char buf[CINI_BUFFER_SIZE];
+		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, sizeof(buf), iniFile);
 		return buf;
 	}
 
 	// No error handling, only use if you plan to handle errors on your own
-	int getInt32Unsafe(const std::string& app, const std::string& key) {
-		char buf[100];
-		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
+	int getInt32Unsafe(const std::string& app, const std::string& key) const {
+		char buf[CINI_BUFFER_SIZE];
+		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, sizeof(buf), iniFile);
 		return std::stoi(buf);
 	}
 
 	// No error handling, only use if you plan to handle errors on your own
-	uint32_t getUint32Unsafe(const std::string& app, const std::string& key) {
-		char buf[100];
-		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
+	uint32_t getUint32Unsafe(const std::string& app, const std::string& key) const {
+		char buf[CINI_BUFFER_SIZE];
+		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, sizeof(buf), iniFile);
 		return std::strtoul(buf, nullptr, 0);
 	}
 
 	// No error handling, only use if you plan to handle errors on your own
-	int64_t getInt64Unsafe(const std::string& app, const std::string& key) {
-		char buf[100];
-		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
+	int64_t getInt64Unsafe(const std::string& app, const std::string& key) const {
+		char buf[CINI_BUFFER_SIZE];
+		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, sizeof(buf), iniFile);
 		return std::stoll(buf);
 	}
 
 	// No error handling, only use if you plan to handle errors on your own
-	uint64_t getUint64Unsafe(const std::string& app, const std::string& key) {
-		char buf[100];
-		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
+	uint64_t getUint64Unsafe(const std::string& app, const std::string& key) const {
+		char buf[CINI_BUFFER_SIZE];
+		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, sizeof(buf), iniFile);
 		return std::stoull(buf);
 	}
 
 	// No error handling, only use if you plan to handle errors on your own
-	float getFloatUnsafe(const std::string& app, const std::string& key) {
-		char buf[100];
-		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
+	float getFloatUnsafe(const std::string& app, const std::string& key) const {
+		char buf[CINI_BUFFER_SIZE];
+		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, sizeof(buf), iniFile);
 		return std::stof(buf);
 	}
 
 	// No error handling, only use if you plan to handle errors on your own
-	bool getBoolUnsafe(const std::string& app, const std::string& key) {
-		char buf[100];
-		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, 100, iniFile.c_str());
+	bool getBoolUnsafe(const std::string& app, const std::string& key) const {
+		char buf[CINI_BUFFER_SIZE];
+		GetPrivateProfileStringA(app.c_str(), key.c_str(), "NULL", buf, sizeof(buf), iniFile);
 		return (std::stoi(buf) ? true : false);
 	}
 
 private:
-	std::string iniFile;
+	const char* iniFile; // maybe this :eyes:
 };
--- README.txt
+++ README.txt
@@ -1,11 +1,22 @@
 [ ----- CIni Information ----- ]
 
 A simple single-header INI class with inbuilt exception handling
-Written by QuickNET
-Latest Version : 1.1.1
-Release Date : 2020/06/24
+Written by : QuickNET
+Latest Version : 1.2.0
+Release Date : 2020/08/05
 
 [ ----- CIni Change Log ----- ]
+
+Version 1.2.0
+ - Added a second constructor taking a const char*
+ - Added macro to customize buffer size
+   - Define `CINI_BUFFER_SIZE` with the value of the desired buffer size before your inclusion of `CIni.h`
+   - If you do not define `CINI_BUFFER_SIZE` it will be defined for you and will be set to 0xFF
+ - Replaced static buffer size in call to `GetPrivateProfileStringA` with `sizeof(buf)`
+ - A little less std::string
+ - A fuck ton more const
+ - Class instances can now be const
+ - Removed tons of redundant code (particularly redundant try/catch statements)
 
 Version 1.1.1
  - Moved CIni Information and CIni Change Log into a separate read me file
